3c870bbfcd2cfbb87ff019408f078c5350a59053
Lindsay Morris <lindsaymorris813@gmail.com>
2020-12-04 21:05:40 -0500

    set up basic server

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log("App listening at http://localhost:" + PORT);
+});
